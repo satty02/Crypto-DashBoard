75f011e6c2d123127bdb37862303d3362618fff0
satty02 <satish.pal.2996@gmail.com>
2023-07-08 16:55:19 +0530

    test files & test cases are added for all components

--- src/Components/BaseCurrency/BaseCurrency.test.js
+++ src/Components/BaseCurrency/BaseCurrency.test.js
@@ -1,77 +1,50 @@
+// in this script BaseCurrency component is tested
+
 import React from 'react';
-import {fireEvent, render,screen} from '@testing-library/react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 import BaseCurrency from './BaseCurrency';
-import {BaseCurrencyAction} from '../../State/Action/BaseCurrencyAction'
+import { BaseCurrencyAction } from '../../State/Action/BaseCurrencyAction';
 
+// Mock the useDispatch and useSelector hooks
+jest.mock('react-redux', () => ({
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
 
-it('render base currency with redux store',()=>{
-    const initialState = '';
-    const mockStore =  configureStore();
-    const store = mockStore(initialState);
-    const {baseCurrency} = render(
-    <Provider store={store}>
-        <BaseCurrency/>
-    </Provider>
-    )
-    
-})
+describe('BaseCurrency', () => {
+  test('renders select element with options and dispatches action on change', () => {
+    // Mock the baseCurrency value from the Redux store
+    const mockBaseCurrency = 'usd';
+    useSelector.mockReturnValue(mockBaseCurrency);
 
-it('render base currency item List',()=>{
-    const initialState = '';
-    const mockStore =  configureStore();
-    const store = mockStore(initialState);
-    const {baseCurrency} = render(
-    <Provider store={store}>
-        <BaseCurrency/>
-    </Provider>
-    )
+    // Mock the dispatch function
+    const mockDispatch = jest.fn();
+    useDispatch.mockReturnValue(mockDispatch);
 
-    const selectItems = screen.getAllByRole('option');
-    expect(selectItems).toHaveLength(4);
-    
-})
+    // Render the BaseCurrency component
+    render(<BaseCurrency />);
 
+    // Verify the rendering of the select element
+    const selectElement = screen.getByTestId('select');
+    expect(selectElement).toBeInTheDocument();
 
-it('on changing dispatched my action',()=>{
-    const initialState = 'inr';
-    const mockStore =  configureStore();
-    const store = mockStore(initialState);
+    // Verify the options in the select element
+    const optionElements = screen.getAllByTestId('select-option');
+    expect(optionElements).toHaveLength(4);
+    expect(optionElements[0].value).toBe('inr');
+    expect(optionElements[0].textContent).toBe('INR');
+    expect(optionElements[1].value).toBe('usd');
+    expect(optionElements[1].textContent).toBe('USD');
+    expect(optionElements[2].value).toBe('eur');
+    expect(optionElements[2].textContent).toBe('EURO');
+    expect(optionElements[3].value).toBe('jpy');
+    expect(optionElements[3].textContent).toBe('YEN');
 
+    // Simulate a change event on the select element
+    fireEvent.change(selectElement, { target: { value: 'eur' } });
 
-    const {getByRole} = render(
-    <Provider store={store}>
-        <BaseCurrency/>
-    </Provider>
-    )
-
-    const selectElement = screen.getByRole('combobox');
-    fireEvent.change(selectElement,{target:{value:'usd'}});
-
-    const actions = store.getActions();
-    expect(actions).toContainEqual(BaseCurrencyAction('usd'))
-})
-
-it('simulate options',()=>{
-    const initialState = 'inr';
-    const mockStore =  configureStore();
-    const store = mockStore(initialState);
-
-
-    const {getByTestId,getAllByTestId} = render(
-    <Provider store={store}>
-        <BaseCurrency/>
-    </Provider>
-    )
-
-    const selectElement = screen.getByTestId('select')
-    fireEvent.change(selectElement,{target:{value:'usd'}});
-
-    let options = screen.getAllByTestId('select-option');
-
-    expect(options[0].selected).toBeFalsy();
-    expect(options[1].selected).toBeTruthy();
-    expect(options[2].selected).toBeFalsy();
-    expect(options[3].selected).toBeFalsy();
-})
+    // Verify that the BaseCurrencyAction was dispatched with the correct value
+    expect(mockDispatch).toHaveBeenCalledWith(BaseCurrencyAction('eur'));
+  });
+});
