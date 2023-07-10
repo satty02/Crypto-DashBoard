8641cc0e1eac3da68d607949d2902fed5b044bb7
satty02 <satish.pal.2996@gmail.com>
2023-07-10 18:02:07 +0530

    ui added

--- src/Components/Charts/BarChart.js
+++ src/Components/Charts/BarChart.js
@@ -4,9 +4,11 @@ import { useSelector } from "react-redux";
 
 const BarChart = () => {
 
+  const baseCurrency = useSelector(state=>state.BaseCurrency)
+
     const chartData = useSelector(state=>state.chartData);
     // selected coin is 
- const selectedCoins = useSelector(state=>state.coinSelect)
+ const selectedCoins = useSelector(state=>state.coinSelect);
 
 
   const generateRandomColor = () => {
@@ -29,18 +31,33 @@ const BarChart = () => {
     label: `${selectedCoins[index]}`,
     backgroundColor: randomColors[index],
     borderColor: randomColors[index],
-    data: data.map(price=>price[1]),
+    data: data.map(price=>`${parseInt(price[1])/1000000000}`),
   }))
 
   const data = {
     labels: labels,
     datasets: chartMultiData,
   };
+
+  const options = {
+    responsive: true, 
+    maintainAspectRatio: false, 
+    scales: {
+      y: {
+        position:'top',
+        title: {
+          display: true,
+          text:`Mkt Cap in Billion ${baseCurrency==='inr'?'₹':baseCurrency==='usd'?'$':baseCurrency==='eur'?'€':baseCurrency==='jpy'?'¥':'No select currency'}`,
+        },
+    },
+  }
+};
+
   return (
-    <div data-testid="bar-chart">
-      <Bar data={data} />
+    <div data-testid="bar-chart" className="w-full h-[18rem]">
+      <Bar data={data} options={options}/>
     </div>
   );
 };
 
-export default BarChart;
+export default BarChart;  
