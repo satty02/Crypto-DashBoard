3876dab951c25d303f665ff37043cdf9c47c5da3
satty02 <satish.pal.2996@gmail.com>
2023-07-11 16:17:55 +0530

    chart component improved

--- src/Components/Charts/BarChart.js
+++ src/Components/Charts/BarChart.js
@@ -3,8 +3,11 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 const BarChart = () => {
+
+  const days = useSelector(state=>state.coinsDate)
 
   const baseCurrency = useSelector(state=>state.BaseCurrency)
 
@@ -25,9 +28,29 @@ const BarChart = () => {
   };
   
   const randomColors = Array.from({ length: chartData.length }).map(() => generateRandomColor());
-  
-  const labels = ['Jan' , 'Feb' , 'Mar' , 'Apr', 'May', 'Jun' , 'Jul' ,'Aug', 'Sep', 'Oct', 'Nov','Dec']
-  
+    
+  const timestamps = chartData.map(time=>time.map(ts=>ts[0]))[0]
+  let uniqueLabels = Array.isArray(timestamps)
+  ? Array.from(
+    new Set(
+      timestamps.map((timestamp) => {
+        if (days == 1) {
+          return moment(parseInt(timestamp)).format("HH:MM");
+        } else if (days <= 7) {
+          return moment(parseInt(timestamp)).format("DD");
+        } else if (days <= 30) {
+          return moment(parseInt(timestamp)).format("DD");
+        } else if (days <= 180) {
+          return moment(parseInt(timestamp)).format("MMM");
+        } else {
+          return moment(parseInt(timestamp)).format("MMM");
+        }
+      })
+    )
+  ):[];
+
+  const labels = uniqueLabels;
+
 
   const chartMultiData = chartData.map((data,index)=>({
     label: `${selectedCoins[index]}`,
@@ -45,6 +68,14 @@ const BarChart = () => {
     responsive: true, 
     maintainAspectRatio: false, 
     scales: {
+      x: {
+        ticks: {
+          maxTicksLimit: 10,
+          autoSkip: true,
+          maxRotation: 0,
+          autoSkipPadding:5 ,
+        },
+      },
       y: {
         position:'top',
         title: {
--- src/Components/Charts/LineChart.js
+++ src/Components/Charts/LineChart.js
@@ -8,6 +8,9 @@ import moment from "moment/moment";
 
 const LineChart = () => {
 
+  const days = useSelector(state=>state.coinsDate)
+
+
     const baseCurrency = useSelector(state=>state.BaseCurrency)
 
     const chartData = useSelector(state=>state.chartData);
@@ -29,15 +32,38 @@ const LineChart = () => {
     const randomColors = Array.from({ length: chartData.length }).map(() => generateRandomColor());
     
     
-    const labels = ['Jan' , 'Feb' , 'Mar' , 'Apr', 'May', 'Jun' , 'Jul' ,'Aug', 'Sep', 'Oct', 'Nov','Dec']
+    // const labels = ['Jan' , 'Feb' , 'Mar' , 'Apr', 'May', 'Jun' , 'Jul' ,'Aug', 'Sep', 'Oct', 'Nov','Dec']
     
+    const timestamps = chartData.map(time=>time.map(ts=>ts[0]))[0]
+
+    let uniqueLabels = Array.isArray(timestamps)
+    ? Array.from(
+      new Set(
+        timestamps.map((timestamp) => {
+          if (days == 1) {
+            return moment(parseInt(timestamp)).format("HH:MM:SS");
+          } else if (days <= 7) {
+            return moment(parseInt(timestamp)).format("HH:MM DD");
+          } else if (days <= 30) {
+            return moment(parseInt(timestamp)).format("HH:MM DD");
+          } else if (days <= 180) {
+            return moment(parseInt(timestamp)).format("DD MMM");
+          } else {
+            return moment(parseInt(timestamp)).format("DD MMM");
+          }
+        })
+      )
+    ):[];
+  
+    const labels = uniqueLabels;
 
     const chartMultiData = chartData.map((data,index)=>({
       label: `${selectedCoins[index]}`,
       backgroundColor: randomColors[index],
       borderColor: randomColors[index],
       data: data.map(price=>`${parseInt(price[1])/1000000000}`),
-      borderWidth:1,
+      borderWidth:2,
+      pointRadius:0
     }))
 
 
@@ -52,6 +78,14 @@ const LineChart = () => {
       responsive: true, 
       maintainAspectRatio: false, 
       scales: {
+        x: {
+          ticks: {
+            maxTicksLimit: 10,
+            autoSkip: true,
+            maxRotation: 0,
+            autoSkipPadding:5,
+          },
+        },
         y: {
           position:"top",
           title: {
